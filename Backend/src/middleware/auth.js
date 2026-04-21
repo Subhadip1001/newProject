6fe592127304216513e0cae09a8ea27f27a3de88
Subhadip Mandal <subhadipmandal714@gmail.com>
2026-04-21 17:44:38 +0530

    update

--- Backend/src/middleware/auth.js
+++ Backend/src/middleware/auth.js
@@ -5,11 +5,13 @@ export const protect = (req, res, next) => {
 
     if(!token){
         return res.status(401).json({ message:"Not authorized"});
-
-        try {
-            const decoded = jwt.verify(token, process.env.JWT_Secret);
-        } catch (error) {
-            res.status(401).json({ message:"Toekn failed"});
-        }
     }
-}
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_Secret);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        res.status(401).json({ message:"Token failed"});
+    }
+};
